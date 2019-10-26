f8d03c39d9e4b828243d15f0464cb87fa483d10f
SilentCLD <sean@amaranthgaming.com>
2019-10-26 15:07:57 +0100

    mk2, this might be a better way

---
+++ HeightmapTerrain/Scripts/GeometricPrimitive.Terrain.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Xenko.Core.Mathematics;
+using Xenko.Graphics;
+
+namespace Xenko.Graphics.GeometricPrimitives
+{
+    public partial class GeometricPrimitive
+    {
+        /// <summary>
+        /// A Terrain Primitive
+        /// </summary>
+        public static class Terrain
+        {
+            public static GeometricMeshData<VertexPositionNormalTexture> New(float sizeX = 1.0f, float sizeZ = 1.0f, Texture heightmap = null, bool generateBackFace = false, float uFactor = 1f, float vFactor = 1f, bool toLeftHanded = false)
+            {
+                bool useRandomHeight = false;
+                int vertexCount = 100;
+                Color[] heightValues = new Color[vertexCount * vertexCount];
+                heightmap.Recreate();
+
+                if (heightmap != null)
+                {
+                    //throw new Exception();
+                    int width = heightmap.CalculateWidth<int>();
+                    if (width > 0)
+                    {
+                        if (heightmap.GraphicsDevice != null)
+                        {
+                            vertexCount = width;
+                            var context = new GraphicsContext(heightmap.GraphicsDevice);
+                            heightmap.GetData(context.CommandList, heightValues);
+                        }
+                    }
+                }
+                else
+                    useRandomHeight = true;
+
+                var vertices = new VertexPositionNormalTexture[vertexCount * vertexCount];
+                var indices = new int[6 * (vertexCount - 1) * (vertexCount - 1)];
+
+                // TODO Calculate
+                Vector3 normal = Vector3.UnitY;
+
+                var uv = new Vector2(uFactor, vFactor);
+
+                Random rng = new Random();
+
+                // Create vertices
+                int vertexPointer = 0;
+                for (int x = 0; x < vertexCount; x++)
+                {
+                    for (int z = 0; z < vertexCount; z++)
+                    {
+                        float height;
+                        if (useRandomHeight)
+                            height = rng.Next(2) / 3.1f;
+                        else
+                            height = heightValues[x + z * vertexCount].R / 3.1f;
+
+                        Vector3 position = new Vector3(x / ((float)vertexCount - 1) * sizeX, height, z / ((float)vertexCount - 1) * sizeZ);
+                        var texCoord = new Vector2(uv.X * x / vertexCount, uv.Y * z / vertexCount);
+
+                        vertices[vertexPointer++] = new VertexPositionNormalTexture(position, normal, texCoord);
+                    }
+                }
+
+                // Create indices
+                int indexPointer = 0;
+                for (int x = 0; x < vertexCount - 1; x++)
+                {
+                    for (int z = 0; z < vertexCount - 1; z++)
+                    {
+                        int topLeft = x + (z + 1) * vertexCount;
+                        int topRight = x + (z + 1) * vertexCount + 1;
+                        int bottomLeft = x + z * vertexCount;
+                        int bottomRight = x + z * vertexCount + 1;
+
+                        indices[indexPointer++] = topLeft;
+                        indices[indexPointer++] = bottomRight;
+                        indices[indexPointer++] = bottomLeft;
+
+                        indices[indexPointer++] = topLeft;
+                        indices[indexPointer++] = topRight;
+                        indices[indexPointer++] = bottomRight;
+                    }
+                }
+
+                // TODO: Generate back faces if needed
+
+                // Create the primitive object.
+                return new GeometricMeshData<VertexPositionNormalTexture>(vertices, indices, toLeftHanded) { Name = "Terrain" };
+            }
+        }
+    }
+}
---
+++ HeightmapTerrain/Scripts/TerrainProceduralModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xenko.Core;
+using Xenko.Core.Mathematics;
+using Xenko.Graphics;
+using Xenko.Graphics.GeometricPrimitives;
+
+namespace Xenko.Rendering.ProceduralModels
+{
+    [DataContract("TerrainProceduralModel")]
+    [Display("Terrain")]
+    public class TerrainProceduralModel : PrimitiveProceduralModelBase
+    {
+        public TerrainProceduralModel()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the size of the terrain.
+        /// </summary>
+        /// <value>The size x.</value>
+        /// <userdoc>The size of terrain along the X/Z axis</userdoc>
+        [DataMember(10)]
+        [Display("Size")]
+        public Vector2 Size { get; set; } = new Vector2(1.0f);
+
+        /// <summary>
+        /// Gets or Sets the heightmap texture
+        /// </summary>
+        /// <userdoc>The heightmap of the terrain</userdoc>
+        [DataMember(20)]
+        [Display("Heightmap")]
+        public Texture Heightmap { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value indicating if a back face should be added.
+        /// </summary>
+        /// <userdoc>Should a back face be generated</userdoc>
+        [DataMember(30)]
+        [DefaultValue(false)]
+        [Display("Back Face")]
+        public bool GenerateBackFace { get; set; }
+
+        protected override GeometricMeshData<VertexPositionNormalTexture> CreatePrimitiveMeshData()
+        {
+            return GeometricPrimitive.Terrain.New(Size.X, Size.Y, Heightmap, GenerateBackFace, UvScale.X, UvScale.Y, false);
+        }
+    }
+}
